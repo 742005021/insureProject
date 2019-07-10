6ba624bbeccc1765818cea8f1705c02f51514b2c
742005021 <742005021@qq.com>
2019-07-10 20:03:33 +0800

    第一次上传

---
+++ custlog-service/src/main/java/org/java/CustlogServiceApplication.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustlogServiceApplication.class, args);
+    }
+
+}
