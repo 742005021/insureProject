d4fb20d29c63da536a7043a1a40497d1d3222929
小飞 <123>
2019-07-11 13:22:21 +0800

    后台管理

---
+++ bgmanage/src/main/java/org/java/BgmanageApplication.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgmanageApplication.class, args);
+    }
+
+}
